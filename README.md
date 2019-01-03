790dedcf81861a7c3df6a34ed6299d90e52c2ed9
Michele Bonazza <michele@sketchtogether.com>
2019-01-03 23:09:08 +0100

    First working version with CSV output

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # amazon-report
 Get your CSV reports back from amazon!
+
+# why
+US users have a nice 'Download report' feature that lets them get a CSV with
+the list of all items they bouth on amazon.com, but unfortunately, the feature
+is not there (yet?) everywhere in the world. At least it doesn't work in
+Germany, or maybe only for my account. This script tries to bring the feature
+to all amazon users.
+
+# running it
+You need to install the Selenium Web Driver for either [Firefox][1] or
+[Chrome/Chromium][2], and make sure that the executable is in your `PATH`
+environment variable. Then, I suggest creating a virtualenv for python, but of
+course it's optional. To install all dependencies, run:
+
+    pip install -r requirements.txt
+
+And finally, run:
+
+    python amazon-report.py
+
+The script will ask for the URL to use (without http:// or https://), the
+driver to use (according to what you installed), and your credentials to fetch
+your data. They're obviously not stored anywhere, see the code for yourself!
+
+[1]: https://github.com/mozilla/geckodriver/releases
+[2]: https://sites.google.com/a/chromium.org/chromedriver/downloads
--- amazon-report.py
+++ amazon-report.py
@@ -4,21 +4,43 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
+import csv
+import getpass
 
-username = 'my_amazon_username'
-password = 'my_amazon_password'
+try:
+    _input = raw_input
+except NameError:
+    _input = input
 
-browser = webdriver.Firefox()
-browser.implicitly_wait(10)
-browser.get('https://www.amazon.de')
+amzn_url = _input('Your amazon account is at [www.amazon.de]: ').strip()
+browser = _input('Selenium driver to use (chrome/firefox) [firefox]: ').strip()
+username = _input('Your username on amazon: ').strip()
+password = getpass.getpass()
+
+print('')
+print('All set! Please do not move your mouse while the script is running.')
+print('Yeah, I know. :/')
+print('')
+_input('Press any key to start the script')
+print('')
+
+print('Sit back, relax, and enjoy your export. Keep your fingers crossed!')
+print('If the script is stuck, just close the {} window'.format(
+    browser if browser else 'firefox'))
+
+browser = webdriver.Chrome() if browser == 'chrome' else webdriver.Firefox()
+browser.implicitly_wait(3)
+browser.get('https://{}'.format(amzn_url if amzn_url else 'www.amazon.de'))
 
 your_account = browser.find_element(By.ID, 'nav-link-yourAccount')
 actions = webdriver.ActionChains(browser)
+sleep(3)
 actions.move_to_element(your_account).perform()
 WebDriverWait(browser, 10).until(
-        EC.presence_of_element_located((By.ID, 'nav-flyout-ya-signin')))
+        EC.visibility_of_element_located((By.ID, 'nav-flyout-ya-signin')))
+sleep(2)
 login = browser.find_element_by_css_selector('#nav-flyout-ya-signin > a')
-actions.move_to_element(login).click().perform()
+login.click()
 
 browser.find_element(By.ID, 'ap_email')
 email = browser.find_element(By.ID, 'ap_email')
@@ -33,31 +55,73 @@ login_btn = browser.find_element(By.ID, 'signInSubmit')
 login_btn.click()
 
 WebDriverWait(browser, 10).until(
-        EC.presence_of_element_located((By.ID, 'nav-link-yourAccount')))
+        EC.visibility_of_element_located((By.ID, 'nav-link-yourAccount')))
 your_acct = browser.find_element_by_css_selector(
         '#nav-link-yourAccount > span.nav-line-1')
 actions = webdriver.ActionChains(browser)
 actions.move_to_element(your_acct).perform()
-sleep(2)
 WebDriverWait(browser, 10).until(
-        EC.presence_of_element_located((By.ID, 'nav_prefetch_yourorders')))
+        EC.visibility_of_element_located((By.ID, 'nav_prefetch_yourorders')))
 orders = browser.find_element(By.ID, 'nav_prefetch_yourorders')
 actions.move_to_element(orders).click().perform()
 
-sleep(2)
-values = browser.find_elements_by_css_selector(
-        'div.a-column.a-span2 > div.a-row.a-size-base > span.value')
-
-for value in values:
-    print(value.get_attribute('textContent'))
+WebDriverWait(browser, 10).until(
+        EC.visibility_of_element_located(
+            (By.CSS_SELECTOR, 'span.a-dropdown-prompt')))
+sleep(3)
 time_frame = browser.find_element_by_css_selector('span.a-dropdown-prompt')
 time_frame.click()
-sleep(2)
 last_year = browser.find_element_by_id('orderFilter_3')
 last_year.click()
 
-values = browser.find_elements_by_css_selector(
-        'div.a-column.a-span2 > div.a-row.a-size-base > span.value')
 
-for value in values:
-    print(value)
+def text(el):
+    return el.get_attribute('textContent').strip()
+
+
+def get_values_and_get_last_button():
+    orders = browser.find_elements_by_css_selector('div.a-box-group.order')
+    values = []
+    for o in orders:
+        date = text(o.find_element(By.CSS_SELECTOR,
+                                   'div.order-info div.a-col-left > div.a-row'
+                                   '> div:nth-child(1) > div.a-size-base'
+                                   '> span.value'))
+        items = o.find_elements_by_css_selector(
+            'div.shipment div.a-row > div.a-fixed-left-grid')
+        for i in items:
+            desc = text(i.find_element(By.CSS_SELECTOR,
+                                       'div.a-col-right > div:nth-child(1)'
+                                       '> a'))
+            cost = text(i.find_element(By.CSS_SELECTOR,
+                                       'div.a-col-right span.a-color-price'
+                                       '> nobr')).split()[1]
+            values.append((date, float(cost.replace(',', '.')),
+                           '{}'.format(unicode(desc).encode('utf-8'))))
+    try:
+        last = browser.find_element_by_css_selector(
+            'div.a-text-center.pagination-full ul.a-pagination li.a-last a')
+    except:
+        last = None
+    return (values, last)
+
+rows = []
+values, last = get_values_and_get_last_button()
+rows.extend(values)
+
+while last:
+    last.click()
+    values, last = get_values_and_get_last_button()
+    rows.extend(values)
+
+print('Done! Writing results to amazon-report.csv...')
+with open('amazon-report.csv', 'w') as file_out:
+    csvwriter = csv.writer(file_out, delimiter=',', quotechar='"',
+                           quoting=csv.QUOTE_MINIMAL)
+    csvwriter.writerow(['Date', 'Price', 'Item description'])
+    for row in rows:
+        csvwriter.writerow(row)
+
+browser.quit()
+
+print('Your CSV is ready!')
